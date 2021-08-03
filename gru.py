f12bde5f1e5a9bf07b31c5c89968293ace8f57b6
Artur <magisterbrownie@gmail.com>
2021-08-03 09:32:58 +0200

    GRU unit

---
+++ gru.py
@@ -0,0 +1,38 @@
+import numpy as np
+import torch.nn as nn
+import torch
+
+class GRU(nn.Module):
+    def __init__(self, inputs: int, hidden: int):
+        super().__init__()
+        self.reset = InputGate(inputs,hidden,nn.Sigmoid())
+        self.update = InputGate(inputs,hidden,nn.Sigmoid())
+        self.candidate = InputGate(inputs,hidden,nn.Tanh())        
+
+
+    def forward(self, x: torch.Tensor, memory: torch.Tensor):
+        reswei = self.reset(x,memory)
+        upwei = self.update(x,memory)
+        
+        mempart = memory*reswei
+        canrem = self.candidate(x,mempart)
+
+        savemem = memory*(1-upwei)
+        newmem = canrem*upwei
+
+        return savemem+newmem
+
+
+class InputGate(nn.Module):
+    def __init__(self, inputs: int, hidden: int, activation: nn.Module):
+        super().__init__()
+
+        self.memory = nn.Linear(hidden,hidden)
+        self.input = nn.Linear(inputs,hidden)
+        self.sig = activation
+
+    def forward(self, x: torch.Tensor, hidden: torch.Tensor):
+
+        x = self.input(x)+self.hidden(x)
+        x = self.sig(x)
+        return x
